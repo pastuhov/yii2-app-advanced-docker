bdf36f56fee2a57a766ba32cbd2ac918ea2f5481
Kirill Pastukhov <kirill@pastukhov.su>
2015-07-08 22:48:42 +0500

    add yii2 app advanced

---
+++ console/migrations/m130524_201442_init.php
@@ -0,0 +1,34 @@
+<?php
+
+use yii\db\Schema;
+use yii\db\Migration;
+
+class m130524_201442_init extends Migration
+{
+    public function up()
+    {
+        $tableOptions = null;
+        if ($this->db->driverName === 'mysql') {
+            // http://stackoverflow.com/questions/766809/whats-the-difference-between-utf8-general-ci-and-utf8-unicode-ci
+            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
+        }
+
+        $this->createTable('{{%user}}', [
+            'id' => Schema::TYPE_PK,
+            'username' => Schema::string()->notNull()->unique(),
+            'auth_key' => Schema::string(32)->notNull(),
+            'password_hash' => Schema::string()->notNull(),
+            'password_reset_token' => Schema::string()->unique(),
+            'email' => Schema::string()->notNull()->unique(),
+
+            'status' => Schema::smallInteger()->notNull()->default(10),
+            'created_at' => Schema::integer()->notNull(),
+            'updated_at' => Schema::integer()->notNull(),
+        ], $tableOptions);
+    }
+
+    public function down()
+    {
+        $this->dropTable('{{%user}}');
+    }
+}
